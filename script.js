4ba1e71e699414499a6db65318edf1705703569c
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2023-11-01 13:01:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Add an event listener to the button
+    var button = document.getElementById("clickMe");
+    var output = document.getElementById("output");
 
+    button.addEventListener("click", function () {
+        output.innerHTML = "Button Clicked!";
+    });
+});
