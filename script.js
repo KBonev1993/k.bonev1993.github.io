6a61449a6ce5a8a8b086b80630d566a10ff87a51
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2023-11-01 13:05:55 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Add an event listener to the button
-    var button = document.getElementById("clickMe");
-    var output = document.getElementById("output");
-
-    button.addEventListener("click", function () {
-        output.innerHTML = "Button Clicked!";
-    });
-});
